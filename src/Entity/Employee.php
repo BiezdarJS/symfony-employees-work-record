a39d085065a92c4bda87bec42e47375557745b26
BiezdarJS <piotrdeja.pl@gmail.com>
2025-04-14 18:02:11 +0200

    Emploee entity - make filed changes to meet endpoint requirements

--- src/Entity/Employee.php
+++ src/Entity/Employee.php
@@ -18,7 +18,10 @@ class Employee
     private ?Uuid $id = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $fullName = null;
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastName = null;
 
     #[ORM\OneToMany(mappedBy: 'employee', targetEntity: WorkDayRecord::class, cascade: ['persist', 'remove'])]
     private Collection $workDayRecords;
@@ -33,14 +36,26 @@ class Employee
         return $this->id;
     }
 
-    public function getFullName(): ?string
+    public function getFirstName(): ?string
     {
-        return $this->fullName;
+        return $this->firstName;
     }
 
-    public function setFullName(string $fullName): static
+    public function setFirstName(string $firstName): static
     {
-        $this->fullName = $fullName;
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): static
+    {
+        $this->lastName = $lastName;
 
         return $this;
     }
